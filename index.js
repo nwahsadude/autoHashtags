e715d1395ee410c20ba442e5a107940051662f80
nwahsadude <nwahsadude@gmail.com>
2017-12-29 19:44:27 -0500

    adding cross domain support

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ let keys = require("./keys");
 
 let app = express();
 
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 app.get('/', function (req, res) {
     res.send('hello world')
 });
